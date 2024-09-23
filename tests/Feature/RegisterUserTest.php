1d6e548353dfc9c9d1b8694707be1649f3437d30
tranthethang <tranthethang@gmail.com>
2024-09-23 17:53:48 +0700

    finish setup php-pest

--- tests/Feature/RegisterUserTest.php
+++ tests/Feature/RegisterUserTest.php
@@ -4,6 +4,5 @@ use function PHPUnit\Framework\assertTrue;
 
 test('basic test', function () {
     $response = $this->post(route('users.register'));
-
     assertTrue(true);
 });
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,14 +6,4 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-        if ('testing' !== config('app.env')) {
-            $msg = 'Testing is only possible in a "testing" environment. Please config APP_ENV=testing, and try again.';
-            info($msg);
-
-            exit();
-        }
-    }
 }
