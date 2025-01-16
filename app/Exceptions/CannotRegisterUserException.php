46f7d1cace260b2fc217c5002614a9753d6cee0f
tranthethang <tranthethang@gmail.com>
2025-01-16 13:16:08 +0700

    update Laravel Pint

--- app/Exceptions/CannotRegisterUserException.php
+++ app/Exceptions/CannotRegisterUserException.php
@@ -8,8 +8,11 @@ use Throwable;
 
 class CannotRegisterUserException extends Exception
 {
-    public function __construct(?string $message = null, int $code = Response::HTTP_INTERNAL_SERVER_ERROR, ?Throwable $previous = null)
-    {
+    public function __construct(
+        ?string $message = null,
+        int $code = Response::HTTP_INTERNAL_SERVER_ERROR,
+        ?Throwable $previous = null
+    ) {
         parent::__construct($message ?? __('exception.cannot_register_user_exception'), $code, $previous);
     }
 }
--- app/Exceptions/NotTrustRequestException.php
+++ app/Exceptions/NotTrustRequestException.php
@@ -8,8 +8,11 @@ use Throwable;
 
 class NotTrustRequestException extends Exception
 {
-    public function __construct(?string $message = null, int $code = Response::HTTP_BAD_REQUEST, ?Throwable $previous = null)
-    {
+    public function __construct(
+        ?string $message = null,
+        int $code = Response::HTTP_BAD_REQUEST,
+        ?Throwable $previous = null
+    ) {
         parent::__construct($message ?? __('exception.request_is_invalid'), $code, $previous);
     }
 }
